35a0d48e9d6d2796500e59b3fe0bbeb28ce40cea
Rahul verma <rahulrocks2051@gmail.com>
2012-04-16 00:44:35 +0530

    added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 	
   def home
+  	@title ="Home"
   end
 
   def contact
+  	@title ="contact"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,36 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+describe PagesController do
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title"do
+        get 'home'
+        response.should have_selector("title",
+          :content => "ROR Sample App | Home")
   end
+    it "should have a non blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+      it "should have the right title"do
+        get 'contact'
+        response.should have_selector("title",
+          :content => "ROR Sample App | contact")
+  end
   end
 
 end
